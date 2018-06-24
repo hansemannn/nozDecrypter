6fa3f82cc36f1362ddcbe92b475e28b37a6a1fdc
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-06-25 01:00:34 +0200

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,25 +1,20 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender) {
-        //alert(request);
-        var element = document.getElementById('content-jump-to');
-        element.classList.remove("blurred");
-        var child = element.children;
-        var zahler = 0;
-        console.log(child);
-        
-        for (var i = 0; i < child.length; i++){
-            //console.log(child[i].tagName);
-            var text = child[i].textContent;
-            var result = "";
-            for (var j = 0; j < text.length; j++){
-                if (text.charCodeAt(j) != 32){
-                    var char = text.charCodeAt(j) + 2;
-                } else {
-                    var char = text.charCodeAt(j);
-                }
-                result += String.fromCharCode(char);
+chrome.runtime.onMessage.addListener((request, sender) => {
+    const element = document.getElementById('content-jump-to');
+    const child = element.children;
+
+    element.classList.remove('blurred');
+
+    for (let i = 0; i < child.length; i++) {
+        const text = child[i].textContent;
+        let result = '';
+
+        for (var j = 0; j < text.length; j++) {
+            let char = text.charCodeAt(j);
+            if (text.charCodeAt(j) !== 32) {
+                char = text.charCodeAt(j) + 2;
             }
-            child[i].innerHTML = result;
+            result += String.fromCharCode(char);
         }
-        console.log(zahler);
-    });
+        child[i].innerHTML = result;
+    }
+});
